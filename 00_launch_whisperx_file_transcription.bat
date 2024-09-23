9186e17642fbb93d6fa4c214742126da833373d8
latent-variable <lino.valdovinos.cs@gmail.com>
2024-09-23 11:41:00 -0700

    fix whisperx_file_transcription

--- 00_launch_whisperx_file_transcription.bat
+++ 00_launch_whisperx_file_transcription.bat
@@ -1,5 +1,5 @@
 @echo off
 CALL ".\installer_files\conda\condabin\conda.bat" activate
 CALL conda activate ".\installer_files\env"
-CALL python ".\extensions\whisperx_stt\script.py"
+CALL python ".\00_my_scripts\whisperx_file_transcription.py"
 cmd /k
